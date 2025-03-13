8b0765e3a4d7dcfc06f401cf219f7b197dc5e2f2
Tin Yadanar <tinyadanar.1997.yu@gmail.com>
2025-03-14 01:37:18 +0700

    added for expanded text

--- lib/presentation/guest_side/view/home/widget/firstrow_section.dart
+++ lib/presentation/guest_side/view/home/widget/firstrow_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_utils/get_utils.dart';
 import 'package:university_magazine_project/app/config/app_color.dart';
 import 'package:university_magazine_project/app/config/app_textstyle.dart';
 
@@ -28,32 +29,58 @@ class FirstRowSection extends StatelessWidget {
                   fit: BoxFit.cover,
                   "https://images.unsplash.com/photo-1571260899304-425eee4c7efc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
               SizedBox(width: 20),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Defined by Excellence\nand Achievement ",
-                    style: AppTextStyle.h3iterBold,
-                    textAlign: TextAlign.start,
-                  ),
-                  SizedBox(height: 5),
-                  Container(
-                    width: 60,
-                    height: 3,
-                    color: Colors.cyan,
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    "We celebrate the varied journeys of our community through our award-winning Greenwich portraits series. \n"
-                    "Get to know your future colleagues and discover their inspiring stories. Our university fosters an inclusive  \n"
-                    "environment where innovation, creativity, and academic excellence thrive. We take pride in our students' and  \n"
-                    "faculty's contributions to research, entrepreneurship, and social impact. From groundbreaking discoveries to  \n"
-                    "transformational learning experiences, every journey at Greenwich is a step towards shaping a brighter future.",
-                    style: AppTextStyle.h3iterBold,
-                    textAlign: TextAlign.start,
-                  ),
-                ],
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Defined by Excellence and\nAchievement ",
+                      style: AppTextStyle.h1iterBold,
+                      textAlign: TextAlign.start,
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      width: 60,
+                      height: 3,
+                      color: Colors.cyan,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      """We celebrate the varied journeys of our community through our award-winning Greenwich portraits series.Get to know your future colleagues and discover their inspiring stories. Our university fosters an inclusive environment where innovation, creativity, and academic excellence thrive. We take pride in our students' and faculty's contributions to research, entrepreneurship, and social impact. From groundbreaking discoveries to transformational learning experiences, every journey at Greenwich is a step towards shaping a brighter future.""",
+                      style: AppTextStyle.h4iterRegular,
+                      textAlign: TextAlign.justify,
+                    ),
+                    MaterialButton(
+                      onPressed: () {},
+                      color: AppColor.whiteColor,
+                      elevation: 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border:
+                              Border(bottom: BorderSide(color: Colors.black)),
+                        ),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Find Your Faculty",
+                              style: AppTextStyle.h3iterRegular,
+                            ),
+                            CircleAvatar(
+                              backgroundColor: AppColor.primaryColor,
+                              child: Icon(
+                                Icons.arrow_forward_ios,
+                                color: AppColor.whiteColor,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    // Text("Find Your Faculty",
+                    //     style: AppTextStyle.h3iterRegular),
+                  ],
+                ),
               )
             ],
           ),
