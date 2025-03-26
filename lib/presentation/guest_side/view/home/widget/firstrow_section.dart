f0743e4495a44f768250d711eff142dcf388d84e
Tin Yadanar <tinyadanar.1997.yu@gmail.com>
2025-03-27 02:04:02 +0700

    responsive 2

--- lib/presentation/guest_side/view/home/widget/firstrow_section.dart
+++ lib/presentation/guest_side/view/home/widget/firstrow_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:university_magazine_project/app/config/app_color.dart';
 import 'package:university_magazine_project/app/config/app_textstyle.dart';
 
@@ -7,88 +8,81 @@ class FirstRowSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 100),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Text(
-          //   "Discover the University of Greenwich",
-          //   style: AppTextStyle.h1iterBold
-          //       .copyWith(fontSize: 30, color: AppColor.primaryColor),
-          // ),
-          // SizedBox(height: 20),
-          Row(
+    return Wrap(
+      alignment: WrapAlignment.start,
+      spacing: 100,
+      crossAxisAlignment: WrapCrossAlignment.start,
+      direction: Axis.horizontal,
+      runSpacing: 20,
+      children: [
+        Image.network(
+            height: 300,
+            width: 450,
+            fit: BoxFit.cover,
+            "https://images.unsplash.com/photo-1571260899304-425eee4c7efc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+        SizedBox(
+          width: (Device.screenType == ScreenType.tablet ||
+                  Device.screenType == ScreenType.desktop)
+              ? 600
+              : 200,
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // SizedBox(width: 70),
-              Image.network(
-                  height: 300,
-                  width: 450,
-                  fit: BoxFit.cover,
-                  "https://images.unsplash.com/photo-1571260899304-425eee4c7efc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-              SizedBox(width: 60),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              Text(
+                "Defined by Excellence and\nAchievement ",
+                style: AppTextStyle.h1iterBold.copyWith(fontSize: 27),
+                textAlign: TextAlign.start,
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: 100,
+                height: 3,
+                color: AppColor.hoverAppBarColor,
+              ),
+              SizedBox(height: 10),
+              Text(
+                """We celebrate the varied journeys of our community through our award-winning Greenwich portraits series.Get to know your future colleagues and discover their inspiring stories. Our university fosters an inclusive environment where innovation, creativity, and academic excellence thrive. We take pride in our students' and faculty's contributions to research, entrepreneurship, and social impact. From groundbreaking discoveries to transformational learning experiences, every journey at Greenwich is a step towards shaping a brighter future.""",
+                style: AppTextStyle.h4iterRegular,
+                textAlign: TextAlign.justify,
+              ),
+              SizedBox(height: 20),
+              MaterialButton(
+                onPressed: () {},
+                color: AppColor.whiteColor,
+                elevation: 0,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      "Defined by Excellence and\nAchievement ",
-                      style: AppTextStyle.h1iterBold.copyWith(fontSize: 27),
-                      textAlign: TextAlign.start,
+                      "Find Your Faculty",
+                      style: (Device.screenType == ScreenType.tablet ||
+                          Device.screenType == ScreenType.desktop)
+                          ? AppTextStyle.h3iterRegular:AppTextStyle.h6iterRegular,
                     ),
-                    SizedBox(height: 10),
-                    Container(
-                      width: 100,
-                      height: 3,
-                      color: AppColor.hoverAppBarColor,
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      """We celebrate the varied journeys of our community through our award-winning Greenwich portraits series.Get to know your future colleagues and discover their inspiring stories. Our university fosters an inclusive environment where innovation, creativity, and academic excellence thrive. We take pride in our students' and faculty's contributions to research, entrepreneurship, and social impact. From groundbreaking discoveries to transformational learning experiences, every journey at Greenwich is a step towards shaping a brighter future.""",
-                      style: AppTextStyle.h4iterRegular,
-                      textAlign: TextAlign.justify,
-                    ),
-                    SizedBox(height: 20),
-                    MaterialButton(
-                      onPressed: () {},
-                      color: AppColor.whiteColor,
-                      elevation: 0,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Find Your Faculty",
-                            style: AppTextStyle.h3iterRegular,
-                          ),
-                          CircleAvatar(
-                            backgroundColor: AppColor.greyLightColor,
-                            maxRadius: 15,
-                            child: Icon(
-                              Icons.arrow_forward_outlined,
-                              size: 20,
-                              color: AppColor.blackColor,
-                            ),
-                          )
-                        ],
+                    CircleAvatar(
+                      backgroundColor: AppColor.greyLightColor,
+                      maxRadius: 15,
+                      child: Icon(
+                        Icons.arrow_forward_outlined,
+                        size: 20,
+                        color: AppColor.blackColor,
                       ),
-                    ),
-                    Divider(
-                      indent: 10,
-                      color: AppColor.blackColor.withOpacity(0.3),
                     )
-                    // Text("Find Your Faculty",
-                    //     style: AppTextStyle.h3iterRegular),
                   ],
                 ),
+              ),
+              Divider(
+                indent: 10,
+                color: AppColor.blackColor.withOpacity(0.3),
               )
+              // Text("Find Your Faculty",
+              //     style: AppTextStyle.h3iterRegular),
             ],
           ),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
--- lib/presentation/guest_side/view/home/widget/secondrow_section.dart
+++ lib/presentation/guest_side/view/home/widget/secondrow_section.dart
@@ -9,67 +9,71 @@ class SecondRowSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 120),
+      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
       width: double.infinity,
       decoration: BoxDecoration(
         color: AppColor.primaryColor,
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Meet our talented \nstudents and alumni",
-                  style: AppTextStyle.h1iterBold
-                      .copyWith(color: AppColor.whiteColor, fontSize: 30),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  width: 100,
-                  height: 2,
-                  color: AppColor.dividerColor,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "We celebrate the varied journeys of our community through our award-winning Greenwich portraits series.Get to know your future colleagues and discover their inspiring stories.",
-                  textAlign: TextAlign.justify,
-                  style: AppTextStyle.h4iterRegular
-                      .copyWith(color: AppColor.whiteColor, fontSize: 15),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                MaterialButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
+      child: Center(
+        child: Wrap(
+          alignment: WrapAlignment.center,
+          spacing: 100,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          direction: Axis.horizontal,
+          runSpacing: 20,
+          children: [
+            SizedBox(
+              width: 500,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Meet our talented \nstudents and alumni",
+                    style: AppTextStyle.h1iterBold
+                        .copyWith(color: AppColor.whiteColor, fontSize: 30),
                   ),
-                  hoverColor: AppColor.hoverAppBarColor,
-                  padding: EdgeInsets.all(18),
-                  color: AppColor.blueColor,
-                  onPressed: () {},
-                  child: Text(
-                    "Find Out More",
-                    style: AppTextStyle.h5poppinsRegular
-                        .copyWith(color: AppColor.whiteColor),
+                  SizedBox(
+                    height: 20,
                   ),
-                ),
-              ],
+                  Container(
+                    width: 100,
+                    height: 2,
+                    color: AppColor.dividerColor,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "We celebrate the varied journeys of our community through our award-winning Greenwich portraits series.Get to know your future colleagues and discover their inspiring stories.",
+                    textAlign: TextAlign.justify,
+                    style: AppTextStyle.h4iterRegular
+                        .copyWith(color: AppColor.whiteColor, fontSize: 15),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  MaterialButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    hoverColor: AppColor.hoverAppBarColor,
+                    padding: EdgeInsets.all(18),
+                    color: AppColor.blueColor,
+                    onPressed: () {},
+                    child: Text(
+                      "Find Out More",
+                      style: AppTextStyle.h5poppinsRegular
+                          .copyWith(color: AppColor.whiteColor),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            width: 50,
-          ),
-          Image.network(
-              height: 400,
-              "https://images.unsplash.com/photo-1543269664-56d93c1b41a6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-        ],
+            Image.network(
+                height: 400,
+                "https://images.unsplash.com/photo-1543269664-56d93c1b41a6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+          ],
+        ),
       ),
     );
   }
