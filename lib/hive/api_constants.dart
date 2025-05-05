27b7df2b71d2d7daae0f7546bf73242ef2532333
htetwai18 <146864556+htetwai18@users.noreply.github.com>
2025-05-05 19:56:31 +0700

    update links

---
+++ lib/hive/api_constants.dart
@@ -0,0 +1,9 @@
+const BASE_URL = "http://localhost:3000";
+const REGISTER = "$BASE_URL/register";
+const LOGIN = "$BASE_URL/login";
+const SUBMIT_CONTRIBUTION = "$BASE_URL/contributions";
+const ADD_COMMENT = "$BASE_URL/reviews";
+const DOWNLOAD = "$BASE_URL/download-selected";
+const ARTICLES = "$BASE_URL/public-articles";
+const REGISTER_GUEST = "$BASE_URL/guests";
+
